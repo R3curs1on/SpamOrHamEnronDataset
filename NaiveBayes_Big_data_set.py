947c8b5b7d175912d69f80776a8d8f29e0496c18
Vedant Shendge (work) <179288721+R3curs1on@users.noreply.github.com>
2024-10-20 00:31:20 +0530

    added image

--- NaiveBayes_Big_data_set.py
+++ NaiveBayes_Big_data_set.py
@@ -1,8 +1,6 @@
 #Import necessary libraries.
 
 import  pandas as pd
-import  numpy as np
-#from scipy.special import result
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
